e07268af15bd8f7dd975d494e04c7ed53c20505d
Orlan <138733592+theorlan@users.noreply.github.com>
2024-10-20 11:13:39 -0500

    Basic programm to determine the student's grade Enter a value and the programm return a grade Learning data type, loops and conditionals

---
+++ basics/grade_students.py
@@ -0,0 +1,30 @@
+#Programm to determine the student's grade.
+grade = 0
+
+while grade != -1:
+  grade = float(input("Please enter the student's grade or enter -1 to exit the program: "))
+  
+  #Check a valid value.
+  if grade < -1:
+    grade = float(input("Please enter the student's grade or enter -1 to exit the program: "))
+
+  else:
+    if grade >= 90:
+      print("The student got an A")
+
+    elif grade >= 80:
+      print("The student got an B")
+
+    elif grade >= 70
+      print("The student got an C")
+
+    elif grade >= 60:
+      print("The student got an D")
+
+    elif grade >= 0:
+      print("The student got an F")
+
+    else:
+      print("You have left the program")
+
+  
